7ca4d6eddf7da90a3fd90dde52ef09aede0e1839
josiane-silwa <josiannesilwa@gmail.com>
2026-08-24 00:58:16 -0300

    Criacao das urls

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,30 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from .views import (UserViewSet, 
+                    ReservaViewSet, 
+                    EventoViewSet,
+                    IngressoViewSet,
+                    PagamentoViewSet,
+                    PortariaViewSet,
+                    AssentoViewSet,
+                    CompartilhamentoIngressoViewSet,
+                    ValidacaoIngressoViewSet,
+                    CatalogoExternoViewSet) 
 
 router = DefaultRouter()
+
+router.register(r"usuarios", UserViewSet, basename='usuario')
+router.register(r"reservas", ReservaViewSet, basename='reserva')
+router.register(r"eventos", EventoViewSet, basename='evento')
+router.register(r"ingressos", IngressoViewSet, basename='ingresso')
+router.register(r"pagamentos", PagamentoViewSet, basename='pagamento')
+router.register(r"assentos", AssentoViewSet, basename='assento')
+router.register(r"compartilhamentos_ingresso", CompartilhamentoIngressoViewSet, basename='compartilhamento')
+router.register(r"catalogo_externo", CatalogoExternoViewSet, basename='catalogo')
+router.register(r"validacoes_ingresso", ValidacaoIngressoViewSet, basename='validacao')
+router.register(r"portaria", PortariaViewSet, basename='portaria')
+
+app_name = "eventos"
 
 urlpatterns = [
     path('', include(router.urls)),
