b9eff9c112ad6d27729cc268f2d6ba1b47ebef78
josiane-silwa <josiannesilwa@gmail.com>
2026-08-25 19:43:01 -0300

    Incluido configuracoes para realizar o deploy do projeto no render

--- backend/settings.py
+++ backend/settings.py
@@ -10,8 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,11 +24,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-ix+nyb3ablx*e%8cot77j23dafb1-ceden0#++cy%cm#2zu+5j'
+DEBUG = config("DJANGO_DEBUG", default=True, cast=bool)
+TICKETMASTER_API_KEY = config("TICKETMASTER_API_KEY", default="")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = config("DJANGO_ALLOWED_HOSTS", default="localhost,127.0.0.1").split(",")
+
+FRONTEND_URL = config("FRONTEND_URL", default="http://localhost:5173")
 
 
 # Application definition
@@ -46,6 +54,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",    
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -55,9 +64,6 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.middleware.locale.LocaleMiddleware',
 ]
-
-# Adicione esta linha no settings.py:
-CSRF_COOKIE_HTTPONLY = False  # Permite que o JavaScript leia o cookie
 
 ROOT_URLCONF = 'backend.urls'
 
@@ -82,18 +88,35 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default='',
+#         conn_max_age=600,
+#         ssl_require=not DEBUG
+#     )
+# }
+
+# --- Banco: Postgres em produção via DATABASE_URL, SQLite no seu PC ---
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        # 'ENGINE': 'django.db.backends.postgresql',
-        # 'NAME': config("DB_NAME"),
-        # 'USER': config("DB_USER"),
-        # 'PASSWORD': config("DB_PASSWORD"),
-        # 'HOST': config("DB_HOST"),
-        # 'PORT': config("DB_PORT"),
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+    )
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+        
+#         # 'ENGINE': 'django.db.backends.postgresql',
+#         # 'NAME': config("DB_NAME"),
+#         # 'USER': config("DB_USER"),
+#         # 'PASSWORD': config("DB_PASSWORD"),
+#         # 'HOST': config("DB_HOST"),
+#         # 'PORT': config("DB_PORT"),
+#     }
+# }
 
 
 # Password validation
@@ -128,6 +151,7 @@ REST_FRAMEWORK = {
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
+    FRONTEND_URL,
 ]
 
 CORS_ALLOW_CREDENTIALS = True
@@ -140,6 +164,13 @@ CSRF_COOKIE_HTTPONLY = False
 SESSION_COOKIE_SAMESITE = 'Lax'
 CSRF_COOKIE_SAMESITE = 'Lax'
 
+if not DEBUG:
+    SESSION_COOKIE_SAMESITE = "None"
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SAMESITE = "None"
+    CSRF_COOKIE_SECURE = True
+    SECURE_SSL_REDIRECT = True
+    
 # Internationalization
 # https://docs.djangoproject.com/en/6.1/topics/i18n/
 
@@ -156,7 +187,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+# --- Estáticos servidos pelo próprio Django via WhiteNoise ---
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
