3b9868d6794fb0e56c618ff57f0d8ccd7ff2e745
josiane-silwa <josiannesilwa@gmail.com>
2026-08-25 19:57:51 -0300

    Arquivos de configuracao para deploy no render

---
+++ Build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# Script de build do backend para o Render.
+set -o errexit
+ 
+pip install -r requirements.txt
+ 
+python manage.py collectstatic --no-input
+python manage.py migrate
+ 
