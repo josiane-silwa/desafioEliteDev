bb069a3afd16d1845bebb195c94e75b2f52a8d86
josiane-silwa <josiannesilwa@gmail.com>
2026-08-26 00:17:18 -0300

    Popula catalogo externo em producao (temporario)

--- build.sh
+++ build.sh
@@ -7,3 +7,4 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py seed_producao 
+python manage.py sincronizar_catalogo --city="Sao Paulo" --keyword="show"
