6ff914bc10d59ddfc3888f4cebd800c249a8d987
josiane-silwa <josiannesilwa@gmail.com>
2026-08-25 23:38:10 -0300

    Usa variavel de ambiente para baseURL da API

--- frontend/src/api/Api.js
+++ frontend/src/api/Api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:8000/app/";
+//const baseURL = "http://localhost:8000/app/";
+const baseURL = import.meta.env.VITE_API_BASE_URL_DEPLOY || "http://localhost:8000/app/";
 
 const Api = axios.create({
   baseURL,
